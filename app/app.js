1f4dbb115bc096ce5d9076e48c968956dfabc84e
ssohma <ssohma0118@gmail.com>
2023-02-26 23:57:09 +0900

    프런트 단 javascript

--- app/app.js
+++ app/app.js
@@ -13,6 +13,8 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine","ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/", home);
 
 module.exports = app;
